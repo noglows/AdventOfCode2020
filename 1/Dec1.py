50b072093a2a6c2c3178bc5ddba481de7f5d3427
noglows <jessica.noglows@outlook.com>
2020-12-09 22:13:30 -0800

    Refactor Dec 1 into single file

--- 1/Dec1.py
+++ 1/Dec1.py
@@ -1,5 +1,21 @@
+# Open and read file
 codes = open("/Users/jess/Documents/Code/Advent2020/1/Data.txt", "r")
 lines = codes.readlines()
+
+# Part 1
+log = {}
+
+for line in lines:
+    line = int(line)
+    if line in log:
+        print("Part One Solution is: ")
+        print(log[line] * line)
+    else:
+        looking_value = 2020 - line 
+        log[looking_value] = line
+
+
+# Part 2
 lines = [int(i) for i in lines]
 
 def find2020():
@@ -9,5 +25,6 @@ def find2020():
                 if x + y + z == 2020:
                     return(x * y * z)
             
-
+print("Part Two Solution is: ")
 print(find2020())
+
