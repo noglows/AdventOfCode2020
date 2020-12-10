ad2eeae7d7225384100574f1e38bf8703b52c24f
noglows <jessica.noglows@outlook.com>
2020-12-09 22:18:13 -0800

    Refactor Dec 2 into single file - additional room for refactoring

--- 2/Dec2_Challenge2.py
+++ 2/Dec2_Challenge2.py
@@ -1,11 +1,35 @@
-# open and read the file
-codes = open("/Users/jess/Documents/Code/Advent2020/2/Data.txt", "r")
-lines = codes.readlines()
+# Open and read the file
+passwords = open("/Users/jess/Documents/Code/Advent2020/2/Data.txt", "r")
+lines = passwords.readlines()
 
+# Part 1
 legal = 0
 
 for line in lines:
-    # split the line into parts
+    # Split the line into parts
+    line_items = line.split(" ")
+
+    # Define a variable for each part of the line
+    string_range = line_items[0].split("-")
+    target = line_items[1][0]
+    password = line_items[2]
+    letter_count = password.count(target)
+
+    # Convert string values to range
+    int_range = range(int(string_range[0]), int(string_range[1])+1)
+
+    # Count the legal passwords
+    if letter_count in int_range:
+        legal += 1
+
+print("Part One Solution is: ")
+print(legal)
+
+# Part Two
+legal = 0
+
+for line in lines:
+    # Split the line into parts
     line_items = line.split(" ")
 
     # define a variable for each part of the line
@@ -20,13 +44,10 @@ for line in lines:
     # count the legal passwords
     in_spot_one = password[spot_one] == target
     in_spot_two = password[spot_two] == target
-    if in_spot_one and in_spot_two:
-        print("not here")
-    elif in_spot_one or in_spot_two:
+    if not(in_spot_one and in_spot_two) and (in_spot_one or in_spot_two):
         legal +=1
-    else:
-        print("not here")
-        
+
+print("Part Two Solution is: ")
 print(legal)
 
 
