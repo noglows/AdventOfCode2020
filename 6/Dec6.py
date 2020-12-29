dd709afce14870f5287f625f38cc4fc191f3fdb4
noglows <jessica.noglows@outlook.com>
2020-12-28 23:19:44 -0500

    Dec 6 refactor

--- 6/Dec6.py
+++ 6/Dec6.py
@@ -2,6 +2,35 @@
 codes = open("/Users/jess/Documents/Code/Advent2020/6/Data.txt", "r")
 lines = codes.readlines()
 
+
+# Part One
+groups = []
+full_line = ""
+yes_sum = 0
+line_count = 0
+
+for line in lines:
+    if line == "\n":
+        groups.append(full_line.replace('\n', ''))
+        full_line = ""
+    elif line == lines[-1]:
+        if full_line == "":
+            groups.append(line.replace('\n', ''))
+        else:
+            full_line += line 
+            groups.append(full_line.replace('\n', ''))
+    else: full_line += line
+
+
+for group in groups:
+    count = len(set(group))
+    yes_sum += count
+
+print("Part One Solution is: ")
+print (yes_sum)
+
+# Part Two
+# open and read the file
 groups = {}
 letter_counts = {}
 full_line = ""
@@ -41,6 +70,7 @@ for i in range(1, line_count+1):
             matches += 1
     letter_counts = {}
 
+print("Part Two Solution is:")
 print (matches)
     
 
