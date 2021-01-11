1c4892d19b74d2c4b1d18803a74e2e0aef9d68a4
noglows <jessica.noglows@outlook.com>
2021-01-11 10:57:18 -0700

    Dec 15 Part One Solution

---
+++ 15/Dec15.py
@@ -0,0 +1,38 @@
+
+# Open and read data
+numbers = open("/Users/jess/Documents/Code/Advent2020/15/Data.txt", "r")
+numbers = numbers.readlines()
+
+# Part One
+game_input = []
+spoken = []
+
+track = {}
+
+game = numbers[0].split(",")
+numbers_length = len(game_input)
+
+turn = 0
+for item in game:
+    spoken.append(int(item))
+    track[int(item)] = turn
+    turn += 1
+
+count = numbers_length + 1
+iterator = 5
+count = iterator + 1
+
+while count < 2020:
+    look = spoken[iterator]
+    if look in track:
+        spoken.append(iterator - track[look])
+    else:
+        spoken.append(0)
+    track[look] = iterator
+    iterator +=1 
+    count += 1
+
+print("Part One Solution is: ")
+print(spoken[-1])
+
+
