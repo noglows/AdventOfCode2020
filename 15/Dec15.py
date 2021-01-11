706dbf0368b60da7c0dbd1e0a9499b8ae3457ec0
noglows <jessica.noglows@outlook.com>
2021-01-11 11:02:32 -0700

    Part two complete - slow, room for optimization

--- 15/Dec15.py
+++ 15/Dec15.py
@@ -35,4 +35,33 @@ while count < 2020:
 print("Part One Solution is: ")
 print(spoken[-1])
 
+game_input = []
+spoken = []
 
+track = {}
+
+game = numbers[0].split(",")
+numbers_length = len(game_input)
+
+turn = 0
+for item in game:
+    spoken.append(int(item))
+    track[int(item)] = turn
+    turn += 1
+
+count = numbers_length + 1
+iterator = 5
+count = iterator + 1
+
+while count < 30000000:
+    look = spoken[iterator]
+    if look in track:
+        spoken.append(iterator - track[look])
+    else:
+        spoken.append(0)
+    track[look] = iterator
+    iterator +=1 
+    count += 1
+
+print("Part Two Solution is: ")
+print(spoken[-1])
